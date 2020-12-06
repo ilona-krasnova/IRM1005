2286083f4fcb81ace6a1a3b04c998adc24289dc5
luskiapierre <72098882+luskiapierre@users.noreply.github.com>
2020-12-06 18:57:53 -0500

    Changes

--- luskia/adopt/adopt.js
+++ luskia/adopt/adopt.js
@@ -1,1 +1,26 @@
 
+
+function myFunction() {
+  document.getElementById("myTextarea").select();
+}
+
+function clicked(e)
+{
+    if(!confirm('Please ensure your form is completed before submitting! If yes please continue!')) {
+        e.preventDefault();
+    }
+}
+
+function createOurTextarea() {
+    var ourElement = document.createElement("TEXTAREA");
+    var ourElementText = document.createTextNode("This is our TextArea!");
+    ourElement.appendChild(ourElementText);
+    document.body.appendChild(ourElement);
+}
+
+
+
+function uncheck() {
+  document.getElementById("homephone").checked = false;
+}
+
