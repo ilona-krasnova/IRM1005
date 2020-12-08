1c4ca7a3db48e9837a32ef1838c91c579bd03ca1
luskiapierre <72098882+luskiapierre@users.noreply.github.com>
2020-12-07 23:43:12 -0500

    Changes

--- luskia/adopt/adopt.js
+++ luskia/adopt/adopt.js
@@ -11,16 +11,34 @@ function clicked(e)
     }
 }
 
-function createOurTextarea() {
-    var ourElement = document.createElement("TEXTAREA");
-    var ourElementText = document.createTextNode("This is our TextArea!");
-    ourElement.appendChild(ourElementText);
-    document.body.appendChild(ourElement);
-}
-
 
 
 function uncheck() {
   document.getElementById("homephone").checked = false;
 }
 
+$(document).ready(function() {
+    var path = window.location.search.split("=")[1];
+    var link = $("#from");
+    if (path === "search"){
+        link.html("Cat-alogue Search");
+        link.attr("href","../../phoebe/search/search.html");
+    }
+});
+
+function myMove() {
+  var elem = document.getElementById("photo");   
+  var pos = 0;
+  var id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+      elem.style.left = pos + "px"; 
+    }
+  }
+}
+
+document.getElementById("para1").style.color = "blue"
--- luskia/team/team.js
+++ luskia/team/team.js
@@ -1,1 +1,69 @@
 
+
+$(document).ready(function(){
+$("p").dblclick(function(){
+$(this).hide();
+});
+});
+
+<p>Click the "Try it" button to check the radio button.</p>
+
+<button onclick="myFunction()">Try it</button>
+
+
+function myFunction() {
+  var x = document.getElementById("myRadio");
+  x.checked = true;
+}
+
+function myFunction() {
+  var x = document.createElement("INPUT");
+  x.setAttribute("type", "radio");
+  document.body.appendChild(x);
+
+
+function myFunction() {
+  document.getElementById("myTextarea").select();
+}
+
+function myFunction() {
+
+  var checkBox = document.getElementById("myCheck");
+  
+  var text = document.getElementById("text");
+
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+  //not working for some reason//
+}
+
+$(document).ready(function(){
+  $("pgtwo").mouseover(function(){
+    $("pgtwo").css("background-color", "yellow");
+  });
+  $("pgtwo").mouseout(function(){
+    $("pgtwo").css("background-color", "lightgray");
+  });
+});
+
+$(document).ready(function(){
+  $("input").focus(function(){
+    $("span").css("display", "inline").fadeOut(2000);
+  });
+});
+</script>
+<style>
+span {
+  display: none;
+}
+</style>
+</head>
+<body>
+
+<input>
+
+<span>Nice to meet you!</span>
+<p>Click in the input field to get focus.</p>
