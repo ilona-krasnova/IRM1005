dc754fa7e0be194747c3efa1fb316ca1d7108710
luskiapierre <72098882+luskiapierre@users.noreply.github.com>
2020-12-09 02:45:00 -0500

    Update adopt.js

--- luskia/adopt/adopt.js
+++ luskia/adopt/adopt.js
@@ -1,8 +1,5 @@
+//TextArea//
 
-
-function myFunction() {
-  document.getElementById("myTextarea").select();
-}
 
 function clicked(e)
 {
@@ -13,32 +10,4 @@ function clicked(e)
 
 
 
-function uncheck() {
-  document.getElementById("homephone").checked = false;
-}
-
-$(document).ready(function() {
-    var path = window.location.search.split("=")[1];
-    var link = $("#from");
-    if (path === "search"){
-        link.html("Cat-alogue Search");
-        link.attr("href","../../phoebe/search/search.html");
-    }
-});
-
-function myMove() {
-  var elem = document.getElementById("photo");   
-  var pos = 0;
-  var id = setInterval(frame, 5);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++; 
-      elem.style.top = pos + "px"; 
-      elem.style.left = pos + "px"; 
-    }
-  }
-}
-
-document.getElementById("para1").style.color = "blue"
+  
