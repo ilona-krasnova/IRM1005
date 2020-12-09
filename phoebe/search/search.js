074afb4f4d6ca151ddf173cbda9c7fe0e80e785b
phoebechong <58110407+phoebechong@users.noreply.github.com>
2020-12-08 20:44:54 -0500

    search jq

--- phoebe/search/search.js
+++ phoebe/search/search.js
@@ -1,64 +1,161 @@
 $(document).ready( function() {
 
-	$("#01").hide();
-	$("#02").hide();
-  $("#03").hide();
-  $("#04").hide();
-  $("#05").hide();
-  $("#06").hide();
-  $("#07").hide();
-  $("#08").hide();
+	$(".all").hide();
+
 });
 
 function showAll(){
-  $("#01").fadeIn();
-  $("#02").fadeIn();
-  $("#03").fadeIn();
-  $("#04").fadeIn();
-  $("#05").fadeIn();
-  $("#06").fadeIn();
-  $("#07").fadeIn();
-  $("#08").fadeIn();
+	$(".all").fadeOut();
+  $("#01").fadeIn(1000);
+  $("#02").fadeIn(1000);
+  $("#03").fadeIn(1000);
+  $("#04").fadeIn(1000);
+  $("#05").fadeIn(1000);
+  $("#06").fadeIn(1000);
+  $("#07").fadeIn(1000);
+  $("#08").fadeIn(1000);
 }
 
 function showGWDog() {
-  $("#03").fadeIn();
-  $("#04").fadeIn();
-  $("#05").fadeIn();
-  $("#08").fadeIn();
+  $(".all").fadeOut();
+	$("#03").fadeIn(1000);
+  $("#04").fadeIn(1000);
+  $("#05").fadeIn(1000);
+  $("#08").fadeIn(1000);
 }
 
 
 function showGWCat() {
-  $("#04").fadeIn();
-  $("#07").fadeIn();
-  $("#08").fadeIn();
+  $(".all").fadeOut();
+	$("#04").fadeIn(1000);
+  $("#07").fadeIn(1000);
+  $("#08").fadeIn(1000);
 }
 
 function showGWkid() {
-  $("#01").fadeIn();
-  $("#04").fadeIn();
-  $("#05").fadeIn();
-  $("#06").fadeIn();
+  $(".all").fadeOut();
+	$("#01").fadeIn(1000);
+  $("#04").fadeIn(1000);
+  $("#05").fadeIn(1000);
+  $("#06").fadeIn(1000);
 }
 
 function showMale() {
-  $("#02").fadeIn();
-  $("#03").fadeIn();
-  $("#06").fadeIn();
-  $("#07").fadeIn();
-  $("#08").fadeIn();
+  $(".all").fadeOut();
+	$("#02").fadeIn(1000);
+  $("#03").fadeIn(1000);
+  $("#06").fadeIn(1000);
+  $("#07").fadeIn(1000);
+  $("#08").fadeIn(1000);
 }
 
 function showFemale() {
-  $("#01").fadeIn();
-  $("#04").fadeIn();
-  $("#05").fadeIn();
+  $(".all").fadeOut();
+	$("#01").fadeIn(1000);
+  $("#04").fadeIn(1000);
+  $("#05").fadeIn(1000);
 }
 
-function showCats() {
-  $("#01").hide();
-  $("#03").hide();
-  $("#05").hide();
-  $("#07").hide();
+function showCat() {
+  $(".all").fadeOut();
+	$("#01").fadeIn(1000);
+  $("#03").fadeIn(1000);
+  $("#05").fadeIn(1000);
+  $("#07").fadeIn(1000);
+}
+
+function showDog() {
+  $(".all").fadeOut();
+  $("#02").fadeIn(1000);
+  $("#04").fadeIn(1000);
+  $("#06").fadeIn(1000);
+  $("#08").fadeIn(1000);
+}
+
+function showSize() {
+	var sizVal = $("#size :selected").val();
+
+	if (sizVal == "small") {
+		$(".all").fadeOut();
+	  $("#01").fadeIn(1000);
+	  $("#03").fadeIn(1000);
+	  $("#05").fadeIn(1000);
+	  $("#07").fadeIn(1000);
+	  $("#08").fadeIn(1000);
+	}
+
+	else if(sizVal == "medium") {
+		$(".all").fadeOut();
+		$("#04").fadeIn(1000);
+	  $("#06").fadeIn(1000);
+	}
+
+	else if(sizVal == "large") {
+		$(".all").fadeOut();
+	  $("#02").fadeIn(1000);
+	}
+}
+
+function showAge() {
+	var ageVal = $("#age").val();
+
+	if (ageVal == 11) {
+		$(".all").fadeOut();
+	  $("#01").fadeIn(1000);
+	}
+
+	else if(ageVal == 2) {
+		$(".all").fadeOut();
+		$("#02").fadeIn(1000);
+	}
+
+	else if(ageVal == 3) {
+		$(".all").fadeOut();
+		$("#03").fadeIn(1000);
+	}
+
+	else if(ageVal == 4) {
+		$(".all").fadeOut();
+		$("#04").fadeIn(1000);
+	}
+
+	else if(ageVal == 5) {
+		$(".all").fadeOut();
+		$("#05").fadeIn(1000);
+	}
+
+	else if(ageVal == 9) {
+		$(".all").fadeOut();
+		$("#06").fadeIn(1000);
+	}
+
+	else if(ageVal == 1) {
+		$(".all").fadeOut();
+		$("#07").fadeIn(1000);
+		$("#08").fadeIn(1000);
+	}
+}
+
+function showAct() {
+	var actVal = $("#activity :selected").val();
+
+	if (actVal == "low") {
+		$(".all").fadeOut();
+	  $("#02").fadeIn(1000);
+		$("#07").fadeIn(1000);
+	}
+
+	else if(actVal == "med") {
+		$(".all").fadeOut();
+		$("#03").fadeIn(1000);
+	  $("#04").fadeIn(1000);
+	}
+
+	else if(actVal == "high") {
+		$(".all").fadeOut();
+	  $("#01").fadeIn(1000);
+		$("#05").fadeIn(1000);
+		$("#06").fadeIn(1000);
+	  $("#08").fadeIn(1000);
+	}
 }
